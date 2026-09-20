e93f699faba5c2564d02ab6e3863f831967d7527
Aaditya Kumar Vishwakarma <aadityakumarvishwakarma31@gmail.com>
2026-09-20 17:34:29 +0530

    Chnaged

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-// async function loadPage(page) {
-
-//     const response = await fetch(`./pages/${page}.html`);
-
-//     const html = await response.text();
-
-//     document.getElementById("app").innerHTML = html;
-
-//     initializePage(page);
-// }
-
-// function initializePage(page) {
-
-//     if (page === "opening") {
-
-//         const video = document.querySelector(".opening-video");
-//         const button = document.querySelector(".tap-button");
-
-//         button.addEventListener("click", () => {
-
-//             video.play();
-
-//             button.style.display = "none";
-
-//         });
-
-//         video.addEventListener("ended", () => {
-//             loadPage("/");
-//         });
-
-//     }
-
-// }
-
-// loadPage("opening");
 
 async function loadPage(page) {
     const response = await fetch(`./pages/${page}.html`);
@@ -43,14 +8,6 @@ async function loadPage(page) {
     initializePage(page);
 }
 
-// async function loadHome() {
-//     const welcome = await fetch("./pages/welcome.html").then(res => res.text());
-//     const meetCouple = await fetch("./pages/meetCouple.html").then(res => res.text());
-
-//     document.getElementById("app").innerHTML =
-//         welcome +
-//         meetCouple;
-// }
 async function loadHome() {
 
     
